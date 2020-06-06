68db53b61453e55acb088f39dfa625819b438e3f
Oseias Ayres <oseias_ayres@hotmail.com>
2020-06-05 20:35:00 -0400

    last updates from NLW

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
+import config from '../config';
 
 class PointsController {
   async index(request: Request, response: Response) {
     const { city, uf, items } = request.query;
+
     const parsedItems = String(items)
       .split(',')
       .map((item) => Number(item.trim()));
+
     const points = await knex('points')
       .join('point_items', 'points.id', '=', 'point_items.point_id')
       .whereIn('point_items.item_id', parsedItems)
@@ -14,7 +17,14 @@ class PointsController {
       .where('uf', String(uf))
       .distinct()
       .select('points.*');
-    return response.json(points);
+
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point, // retorna todos os dados do ponto
+        image_url: `${config.serverURL}/uploads/${point.image}`,
+      };
+    });
+    return response.json(serializedPoints);
   }
   async show(request: Request, response: Response) {
     const { id } = request.params;
@@ -23,11 +33,17 @@ class PointsController {
     if (!point) {
       return response.status(400).json({ message: 'Point not found.' });
     }
+
+    const serializedPoint = {
+      ...point, // retorna todos os dados do ponto
+      image_url: `${config.serverURL}/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
   async create(request: Request, response: Response) {
     const {
@@ -44,8 +60,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image:
-        'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -58,12 +73,15 @@ class PointsController {
     const insertedIds = await trx('points').insert(point);
 
     const point_id = insertedIds[0];
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
     await trx('point_items').insert(pointItems);
     await trx.commit();
     '';
